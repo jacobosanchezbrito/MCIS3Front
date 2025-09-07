f631670a840ff724dc982290ab0194660ac4f1fc
julianscp <sbjacobo4@gmail.com>
2025-09-07 16:15:36 -0500

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,9 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off', // Desactivar la regla no-explicit-any
+    },
   },
 ];
 
