4771b7bcf8b9f9c137d72035102e523723e95c9c
julianscp <sbjacobo4@gmail.com>
2025-09-07 16:39:52 -0500

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000", 
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://mcis3back-production.up.railway.app", 
   withCredentials: true, // importante si trabajas con JWT en cookies
 });
 
