6a7734d4faf81a03296a10e992ea30c14190e174
jacobosanchezbrito <sbjacobo4@gmail.com>
2025-09-21 21:52:18 -0500

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "https://mcis3back-production.up.railway.app", 
-  withCredentials: true, // importante si trabajas con JWT en cookies
+  withCredentials: true, 
 });
 
 export default api;
